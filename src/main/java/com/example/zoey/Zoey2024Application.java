14cdab2749dcbf0f112b33a217eb2920e2c97a8a
zoey <juhyelee@lgcns.com>
2024-09-26 10:52:45 +0900

    Spring Initializer : 프로젝트 기본세팅

---
+++ src/main/java/com/example/zoey/Zoey2024Application.java
@@ -0,0 +1,13 @@
+package com.example.zoey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zoey2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zoey2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zoey/Zoey2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zoey2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
