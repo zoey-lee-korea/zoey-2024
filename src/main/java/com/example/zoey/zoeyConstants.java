08950bca0c774cfbc1a705c1fcb04ed84c5ec5cf
zoey <juhyelee@lgcns.com>
2024-09-27 14:36:24 +0900

    Lombok(@Data,@Builder,@UtilityClass), @JsonSerialize, public static final

--- src/main/java/com/example/zoey/zoeyConstants.java
+++ src/main/java/com/example/zoey/zoeyConstants.java
@@ -7,4 +7,20 @@ public class zoeyConstants {
 	public static final String DEFAULT_SUCCESS_CODE = "SUCCESS";
 	public static final String DEFAULT_SUCCESS_DESC = "Success Response";
 	public static final String SUCCESS_FLAG = "Y";
+
+
+	/*
+	public, static, final은 Java에서 변수를 선언할 때 사용하는 키워드들입니다. 이들은 각각 다음과 같은 의미를 가집니다:
+
+	public: 이 키워드가 붙은 멤버는 어디서든 접근이 가능하다는 것을 의미합니다. 즉, 다른 클래스에서도 이 변수를 참조할 수 있습니다.
+
+	static: 이 키워드가 붙은 멤버는 클래스 멤버임을 의미합니다.
+	즉, 이 변수는 클래스의 인스턴스가 아닌 클래스 자체에 속하며, 클래스의 모든 인스턴스가 이 변수를 공유합니다.
+	따라서 인스턴스를 생성하지 않고도 이 변수를 사용할 수 있습니다.
+
+	final: 이 키워드가 붙은 변수는 한 번 초기화되면 그 값을 변경할 수 없다는 것을 의미합니다. 즉, 이 변수는 상수가 됩니다.
+
+	따라서 public static final 키워드를 사용하여 변수를 선언하면, 이 변수는 공개적으로 접근 가능하고 변경 불가능한 클래스 수준의 상수가 됩니다.
+	이런 특성은 설정 값, 버전 정보, 고정된 메시지 등 프로그램 전체에서 공유하고 변경되지 않아야 하는 값에 사용됩니다
+	 */
 }
